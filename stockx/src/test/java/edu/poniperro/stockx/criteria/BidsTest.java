4a04e3d2172f6909bd48f6c6d01edb90d9c75f87
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-16 22:16:46 +0100

    test(checkCriteriaBidListSize): add more objects to offers list

--- stockx/src/test/java/edu/poniperro/stockx/criteria/BidsTest.java
+++ stockx/src/test/java/edu/poniperro/stockx/criteria/BidsTest.java
@@ -2,6 +2,7 @@ package edu.poniperro.stockx.criteria;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import edu.poniperro.stockx.item.Ask;
 import edu.poniperro.stockx.item.Bid;
 import edu.poniperro.stockx.item.Sneaker;
 
@@ -15,12 +16,17 @@ public class BidsTest {
         Bid bid = new Bid("6", 200);
         Bid bid1 = new Bid("6", 200);
         Bid bid2 = new Bid("6", 200);
-
+        Ask ask = new Ask("6", 200);
+        Ask ask1 = new Ask("6", 200);
+        Ask ask2 = new Ask("6", 200);
         Criteria bids = new Bids();
 
         sneaker.add(bid);
         sneaker.add(bid1);
         sneaker.add(bid2);
+        sneaker.add(ask);
+        sneaker.add(ask1);
+        sneaker.add(ask2);
 
         assertEquals(3, bids.checkCriteria(sneaker).size());        
     }
