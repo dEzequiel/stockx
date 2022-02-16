642ccec07a330386a4cdc6a987904487b8a27008
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-16 01:38:11 +0100

    test(checkOffersSize): check offers list collection size when new object are added

--- stockx/src/test/java/edu/poniperro/stockx/item/OffersCollectionTest.java
+++ stockx/src/test/java/edu/poniperro/stockx/item/OffersCollectionTest.java
@@ -7,10 +7,16 @@ public class OffersCollectionTest {
     @Test
     public void checkOffersSize() {
         Bid bid = new Bid("6", 200);
+        Ask ask = new Ask("6", 200);
+        Sale sale = new Sale("6", 200);
+
+
         Sneaker sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
 
         sneaker.add(bid);
-        assertEquals(1, sneaker.offersSize());
+        sneaker.add(ask);
+        sneaker.add(sale);
+        assertEquals(3, sneaker.offersSize());
     }
     
 }
