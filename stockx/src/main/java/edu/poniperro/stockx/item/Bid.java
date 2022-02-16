583c28fcab83730e88d4a321a1740e827611210e
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-16 01:28:32 +0100

    feat(Bid): create offer type bid

---
+++ stockx/src/main/java/edu/poniperro/stockx/item/Bid.java
@@ -0,0 +1,21 @@
+package edu.poniperro.stockx.item;
+
+public class Bid implements Offer{
+    private String size;
+    private Integer bid;
+
+    public Bid(String size, int bid) {
+        this.size = size;
+        this.bid = bid;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.value();
+    }
+}
