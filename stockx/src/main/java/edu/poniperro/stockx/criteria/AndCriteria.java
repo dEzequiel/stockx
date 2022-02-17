8c8de487b3243825a052753bb5087376de362880
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-17 15:23:53 +0100

    refactor(checkCriteria): compare size instead of concatenate lists

--- stockx/src/main/java/edu/poniperro/stockx/criteria/AndCriteria.java
+++ stockx/src/main/java/edu/poniperro/stockx/criteria/AndCriteria.java
@@ -1,7 +1,9 @@
 package edu.poniperro.stockx.criteria;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import edu.poniperro.stockx.item.Item;
 import edu.poniperro.stockx.item.Offer;
@@ -9,20 +11,24 @@ import edu.poniperro.stockx.item.Offer;
 public class AndCriteria implements Criteria{
     
     private Criteria criteria;
-    private Criteria otheCriteria;
+    private Criteria otherCriteria;
 
     public AndCriteria(Criteria criteria, Criteria otheCriteria) {
         this.criteria = criteria;
-        this.otheCriteria = otheCriteria;
+        this.otherCriteria = otheCriteria;
     }
 
     @Override
     public List<Offer> checkCriteria(Item item) {
 
-        List<Offer> criteriaList = this.criteria.checkCriteria(item);
-        List<Offer> otherCriteriaList = this.otheCriteria.checkCriteria(item);
-        
-        return criteriaList.stream().filter(otherCriteriaList::contains).collect(Collectors.toList());
+        List<Offer> lista_anotherCriteria = new ArrayList<Offer>(otherCriteria.checkCriteria(item));
+        List<Offer> lista_criteria = new ArrayList<Offer>(criteria.checkCriteria(item));
+
+        Stream<Offer> lista_filtrada = lista_anotherCriteria.
+                stream().
+                filter(o -> o.size().equals(lista_criteria.get(0).size()));
+
+        return lista_filtrada.collect(Collectors.toList());
         
     }
 }
