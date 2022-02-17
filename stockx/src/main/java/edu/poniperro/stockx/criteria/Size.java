581075d9e6745b21314a91adecebc1742259915c
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-17 01:51:34 +0100

    feat(Size): add method to return sneakers offers list filtered by size

---
+++ stockx/src/main/java/edu/poniperro/stockx/criteria/Size.java
@@ -0,0 +1,28 @@
+package edu.poniperro.stockx.criteria;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import edu.poniperro.stockx.item.Item;
+import edu.poniperro.stockx.item.Offer;
+
+public class Size implements Criteria{
+    
+    private String size;
+
+    public Size(String size) {
+        this.size = size;
+    }
+
+    private String getSize() {
+        return this.size;
+    }
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        return item.offers().stream()
+                                    .filter(i -> i.size() == getSize())
+                                    .collect(Collectors.toList());
+    }   
+}
